1d16775437c94f4a5b64f21ac02d2ecd4961093f
logeshms.cbe@gmail.com <logeshms.cbe@gmail.com>
2025-10-08 21:54:19 +0530

    first fix

--- api/mywebsite/wsgi.py
+++ api/mywebsite/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for mywebsite project.
-
-It exposes the WSGI callable as a module-level variable named ``app``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
 import sys
 from pathlib import Path
