22c35b7c2b60afaef335197ef8931864330aad31
logeshms.cbe@gmail.com <logeshms.cbe@gmail.com>
2025-10-12 17:03:58 +0530

    favicon redirect route

--- api/mywebsite/urls.py
+++ api/mywebsite/urls.py
@@ -2,11 +2,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from homepage.sitemaps import generate_sitemap_xml
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include(('homepage.urls', 'homepage'), namespace='homepage')),
     path('auth/', include('auth_app.urls')),
     path('sitemap.xml', generate_sitemap_xml, name='sitemap'),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)),
+    path('favicon.png', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)),
 ]
 
 if settings.DEBUG:
