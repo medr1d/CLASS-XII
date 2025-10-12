3b5875293474ec3fb2e709d2e75668e6384fd8ad
Logesh M.S <logeshms.cbe@gmail.com>
2025-10-12 08:08:36 +0000

    hayaku

--- api/auth_app/views.py
+++ api/auth_app/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.db import IntegrityError
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 from homepage.models import UserProfile
 import json
 import re
@@ -193,8 +193,8 @@ def admin_panel_view(request):
         'superusers_count': super_count,
     })
 
+@csrf_exempt
 @login_required
-@csrf_protect
 @require_http_methods(["POST"])
 def update_paid_status(request):
     if not request.user.is_superuser:
